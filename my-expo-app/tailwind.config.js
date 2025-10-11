5fbfcc5bd38bf93fe4cfe3ab59c03654ee746e5e
LuciferM12 <rojasrodriguezmartinomar@gmail.com>
2025-10-11 15:55:10 -0600

    feat: define colors

--- my-expo-app/tailwind.config.js
+++ my-expo-app/tailwind.config.js
@@ -1,10 +1,22 @@
+import { COLORS } from './constants/colors';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: COLORS.primaryDark,
+          light: COLORS.primaryLight,
+        },
+        gold: COLORS.gold,
+        lightGray: COLORS.lightGray,
+        darkGray: COLORS.darkGray, 
+      }
+    },
   },
   plugins: [],
 };
