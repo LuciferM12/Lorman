2aa1b86440e45e53608c0236943ac10d5edae1af
LuciferM12 <rojasrodriguezmartinomar@gmail.com>
2025-10-18 12:42:40 -0600

    feat: add products controller

---
+++ lorman-backend/src/controllers/products.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import { productRegisterSchema, ProductSchema } from "../interfaces/products.interface";
+import ProductService from "../services/products.service";
+
+const ProductController = {
+    async createProduct(req: Request, res: Response) {
+        try {
+            const parsed = productRegisterSchema.safeParse(req.body);
+            if (!parsed.success) {
+                return res.status(400).json({ error: "Solicitud de datos inválida", details: parsed.error.flatten().fieldErrors })
+            }
+
+            const newProduct = await ProductService.createProduct(parsed.data);
+            res.status(201).json({ message: "Producto creado exitosamente", product: newProduct });
+        } catch (error: any) {
+            res.status(400).json({ message: error.message });
+        }
+    },
+
+    async getAll(req: Request, res: Response) {
+        try {
+            const products = await ProductService.listProducts();
+            res.status(200).json({ products });
+        } catch (error: any) {
+            res.status(400).json({ message: error.message });
+        }
+    },
+
+    async updateProduct(req: Request, res: Response) {
+        try {
+            if (!req.params.id_producto) {
+                return res.status(400).json({ error: "El ID del producto es requerido" });
+            }
+            const productId = parseInt(req.params.id_producto, 10);
+            const updatedData = req.body;
+            const parsed = ProductSchema.safeParse(updatedData);
+            if (!parsed.success) {
+                return res.status(400).json({ error: "Solicitud de datos inválida", details: parsed.error.flatten().fieldErrors })
+            }
+            const updatedProduct = await ProductService.updateProduct(productId, updatedData);
+            res.status(200).json({ message: "Producto actualizado exitosamente", product: updatedProduct });
+        } catch (error: any) {
+            res.status(400).json({ message: error.message });
+        }
+    },
+
+    async deleteProduct(req: Request, res: Response) {
+        try {
+            if (!req.params.id_producto) {
+                return res.status(400).json({ error: "El ID del producto es requerido" });
+            }
+            const productId = parseInt(req.params.id_producto, 10);
+            await ProductService.deleteProduct(productId);
+            res.status(200).json({ message: "Producto eliminado exitosamente" });
+        } catch (error: any) {
+            res.status(400).json({ message: error.message });
+        }
+    }
+}
+
+export default ProductController;
