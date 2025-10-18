acf8d3fe0e68ec3221cec2a196a83b60c1e71920
Martín Omar Rojas Rodríguez <160383940+LuciferM12@users.noreply.github.com>
2025-10-18 13:02:21 -0600

    chore: remove comments

--- lorman-client/components/custom/cart/cartSummary.tsx
+++ lorman-client/components/custom/cart/cartSummary.tsx
@@ -1,4 +1,3 @@
-// components/cart/CartSummary.tsx
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Text } from '@/components/ui/text';
