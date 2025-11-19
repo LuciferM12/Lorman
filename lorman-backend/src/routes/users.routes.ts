ba7440fdc0f01ea574ac69b4eb95b386c727e413
LuciferM12 <rojasrodriguezmartinomar@gmail.com>
2025-11-19 14:15:29 -0600

    feat: update component and routes to retrieve users

--- lorman-backend/src/routes/users.routes.ts
+++ lorman-backend/src/routes/users.routes.ts
@@ -7,5 +7,6 @@ userRoutes.post("/register", UsersController.register);
 userRoutes.post("/login", UsersController.login);
 userRoutes.get("/email/:email", UsersController.getUserByEmail);
 userRoutes.put("/:id", UsersController.updateUser);
+userRoutes.get("/", UsersController.listUsers);
 
 export default userRoutes;
